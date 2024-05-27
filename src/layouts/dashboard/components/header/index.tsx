a4dc453eb34ad6c2b851d376f4cc656c4520984a
Your Name <your-email@example.com>
2024-05-27 12:31:04 +0530

    Initial commit

---
+++ src/layouts/dashboard/components/header/index.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import MENU_ITEMS from "../../dashboard.hooks";
+import Notifications from "./Notifications";
+import MobileMenu from "./MobileMenu";
+import TabletMenu from "./TabletMenu";
+
+const Header = () => {
+  return (
+    <>
+      <header className="w-full   min-w-0 flex justify-between px-5 py-2.5 items-center max-h-12 gap-[86px] sm:py-3 sm:px-8 sm:max-h-[76px] lg:max-h-[64px] lg:border-b border-b-contact-shades-gray-1 lg:justify-end">
+        <Link to="/contacts" className="scale-95 lg:hidden">
+          {/* display the logo  */}
+          <img
+            src="/contact-logo.png"
+            alt="logo"
+            className="mb-2 object-contain w-[74px] h-[37px] sm:w-[92px] sm:h-[47px]"
+          />
+        </Link>
+        <div className="hidden flex-1 md:flex lg:hidden">
+          <TabletMenu menuOptions={MENU_ITEMS} />
+        </div>
+        <div className="hidden lg:flex items-center justify-end gap-9">
+          <Notifications />
+        </div>
+        <div className="md:hidden">
+          {/* display the mobile menu */}
+          <MobileMenu menuOptions={MENU_ITEMS} />
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
